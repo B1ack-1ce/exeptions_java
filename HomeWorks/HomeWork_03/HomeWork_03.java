e68e23f82cf55966417f7bfe6250d300a028add6
Artemiy <aptem64@yandex.ru>
2023-08-08 12:33:07 +0300

    added sumArr method

--- HomeWorks/HomeWork_03/HomeWork_03.java
+++ HomeWorks/HomeWork_03/HomeWork_03.java
@@ -2,10 +2,22 @@ package HomeWorks.HomeWork_03;
 
 public class HomeWork_03 {
     public static void main(String[] args) {
-        
+        int[] nums = {3,6,8,10,5};
+
+        System.out.println(isEvenNum(nums[2]));
+
+        System.out.println(sumArr(nums));
     }
 
     private static boolean isEvenNum(int num){
         return num % 2 == 0;
     } 
+
+    private static int sumArr(int[] arr){
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
 }
